b28d11f4594fa7c12a43a51c2b2e5b24fb518fbf
Dmytro Kaleniuk <dimaa.kaleniuk@gmail.com>
2020-08-28 14:52:59 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn JAVA
+          Learn react if you can!
         </a>
       </header>
     </div>
